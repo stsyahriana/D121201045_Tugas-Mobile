6fe5726c8aff9b8b5f5e1a56f2a566676dcd8a5c
stsyahriana <stsyahriana07@gmail.com>
2022-12-14 22:50:52 +0800

    tugas mobile

---
+++ app/src/main/java/com/d121201045/NinaReminder/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.d121201045.NinaReminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.d121201045.NinaReminder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), TaskItemClickListener
+{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/d121201045/NinaReminder/TaskItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.d121201045.NinaReminder
+
+interface TaskItemClickListener
+{
+    fun editTaskItem(taskItem: TaskItem)
+    fun completeTaskItem(taskItem: TaskItem)
+}
